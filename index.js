1ab15e3cfc4f407570f5ea3d17b569fe4c3ae3da
Jan Buschtöns <jan@buschtoens.me>
2018-12-17 17:48:59 +0100

    feat: add `keepExtension` option

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-'use strict';
+"use strict";
 
-const Filter = require('broccoli-persistent-filter');
-const gql = require('graphql-tag');
+const Filter = require("broccoli-persistent-filter");
+const gql = require("graphql-tag");
 
 gql.disableFragmentWarnings();
 
 module.exports = class GraphQLFilter extends Filter {
-  constructor(inputNode, options) {
+  constructor(inputNode, { keepExtension = false, ...options }) {
     super(inputNode, options);
-    this.extensions = ['graphql', 'gql'];
-    this.targetExtension = 'js';
+    this.targetExtension = keepExtension ? null : "js";
+    this.extensions = ["graphql", "gql"];
+  }
+
+  getDestFilePath(relativePath) {
+    const newPath = super.getDestFilePath(relativePath);
+    if (!newPath || this.targetExtension) {
+      return newPath;
+    }
+
+    return `${newPath}.js`;
   }
 
   processString(source) {
@@ -18,14 +27,16 @@ module.exports = class GraphQLFilter extends Filter {
       `export default doc;`
     ];
 
-    source.split('\n').forEach((line, i) => {
+    source.split("\n").forEach((line, i) => {
       let match = /^#import\s+(.*)/.exec(line);
       if (match && match[1]) {
         output.push(`import dep${i} from ${match[1]};`);
-        output.push(`doc.definitions = doc.definitions.concat(dep${i}.definitions);`);
+        output.push(
+          `doc.definitions = doc.definitions.concat(dep${i}.definitions);`
+        );
       }
     });
 
-    return output.join('\n') + '\n';
+    return output.join("\n") + "\n";
   }
-}
+};
